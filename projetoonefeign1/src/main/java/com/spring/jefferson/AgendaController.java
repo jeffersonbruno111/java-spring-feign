20be40569ca896bee7d5d924d210dc74e020ed72
jeffersonbruno111 <jeffbruno123@gmail.com>
2022-03-08 00:00:21 -0300

    Server1 Client2

---
+++ projetoonefeign1/src/main/java/com/spring/jefferson/AgendaController.java
@@ -0,0 +1,19 @@
+package com.spring.jefferson;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("contato")
+public class AgendaController {
+
+	@GetMapping
+	public Contato retornaContato() {
+		return Contato.builder()
+				.id(1L)
+				.nome("Jefferson")
+				.telefone("99999-9999")
+				.build();
+	}
+}
---
+++ projetoonefeign1/src/main/java/com/spring/jefferson/Projetoonefeign1Application.java
@@ -0,0 +1,13 @@
+package com.spring.jefferson;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Projetoonefeign1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Projetoonefeign1Application.class, args);
+	}
+
+}
